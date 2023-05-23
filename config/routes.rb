4242c71929813f3cdc64472678736c9d9bf67d4b
Michael-Bradley Timm <michaelbtimm@gmail.com>
2023-05-23 14:11:47 +0200

    Added the code to perform CRUD actions on the tasks list

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :tasks
+  # # CREATE
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+  # # READ ALL
+  # get 'tasks', to: 'tasks#tasks'
+  # # READ ONE
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # # UPDATE
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # # DELETE
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
